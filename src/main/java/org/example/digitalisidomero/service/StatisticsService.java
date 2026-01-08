04c87a81eccf2ec5a440802f067301f2a2a872b1
SztanyiGy <gyuriii2002@gmail.com>
2026-01-08 13:50:40 +0100

    trakcer-5

--- src/main/java/org/example/digitalisidomero/service/StatisticsService.java
+++ src/main/java/org/example/digitalisidomero/service/StatisticsService.java
@@ -179,29 +179,19 @@ public class StatisticsService {
 
     /**
      * Formázott idő string (óra:perc formátumban)
+     * @deprecated Használd helyette: TimeFormatter.formatDuration()
      */
+    @Deprecated
     public static String formatDuration(long seconds) {
-        long hours = seconds / 3600;
-        long minutes = (seconds % 3600) / 60;
-        long secs = seconds % 60;
-
-        if (hours > 0) {
-            return String.format("%dó %dp", hours, minutes);
-        } else if (minutes > 0) {
-            return String.format("%dp %dmp", minutes, secs);
-        } else {
-            return String.format("%dmp", secs);
-        }
+        return org.example.digitalisidomero.util.TimeFormatter.formatDuration(seconds);
     }
 
     /**
      * Formázott idő string (részletes: óra:perc:másodperc)
+     * @deprecated Használd helyette: TimeFormatter.formatDurationDetailed()
      */
+    @Deprecated
     public static String formatDurationDetailed(long seconds) {
-        long hours = seconds / 3600;
-        long minutes = (seconds % 3600) / 60;
-        long secs = seconds % 60;
-
-        return String.format("%02d:%02d:%02d", hours, minutes, secs);
+        return org.example.digitalisidomero.util.TimeFormatter.formatDurationDetailed(seconds);
     }
 }
